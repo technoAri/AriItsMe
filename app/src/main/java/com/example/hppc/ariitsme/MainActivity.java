9039de5909ae0f9e748b9db33023b5af0788a8b5
Arindam <iamari.cse@gmail.com>
2016-08-26 23:55:14 +0530

    home screen activity added

--- app/src/main/java/com/example/hppc/ariitsme/MainActivity.java
+++ app/src/main/java/com/example/hppc/ariitsme/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.hppc.ariitsme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView nextTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nextTV = (TextView)findViewById(R.id.nextPress);
+
+        nextTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomeScreenActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
