bb8bf79c3126f5adb7e724926daccf553e667bd2
Arindam <iamari.cse@gmail.com>
2016-08-31 23:54:45 +0530

    View Pager added in Experience

--- app/src/main/java/com/example/hppc/ariitsme/Activities/MainActivity.java
+++ app/src/main/java/com/example/hppc/ariitsme/Activities/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.hppc.ariitsme.R;
@@ -17,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        nextTV = (TextView)findViewById(R.id.nextPress);
+        nextTV = (Button)findViewById(R.id.nextPress);
 
         nextTV.setOnClickListener(new View.OnClickListener() {
             @Override
